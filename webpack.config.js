0fcf2d37546c1d47dd3728de05ab822c34c2188a
Ernie Park <epark@hubspot.com>
2016-01-02 14:13:16 -0500

    deploy  netlify prod

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
     entry: [
       'webpack/hot/dev-server',
       'webpack-dev-server/client?http://localhost:8080',
-      path.resolve(__dirname, 'app/js/entry.js')
+      path.resolve(__dirname, 'app/js/entry.js'),
+      path.resolve(__dirname, 'build/index.html')
     ],
     output: {
         path: path.resolve(__dirname, 'build'),
