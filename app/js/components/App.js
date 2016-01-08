7b77cd2a5b44530126b330cb1fa76d0ee547cea7
Ernie Park <epark@hubspot.com>
2016-01-08 02:24:27 -0500

    add dataset for 2015, dynamic styling, etc

--- app/js/components/App.js
+++ app/js/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react/addons';
+import BracketData from '../data/brackets';
 
 export default React.createClass({
 
@@ -6,18 +7,115 @@ export default React.createClass({
     React.addons.LinkedStateMixin
   ],
 
+  filingTypes: {
+    SINGLE: "Single filers",
+    JOINT_OR_WIDOW: "Married filing jointly or qualifying widow(er)",
+    SEPARATE: "Married filing separately",
+    HOH: "Head of household"
+  },
+
   getInitialState() {
     return {
       year: '2015',
+      taxableIncome: '50000',
+      filingType: "SINGLE"
     };
   },
 
-  render() {
+  rateToString({LOWER, UPPER}) {
+    if (LOWER === 0) {
+      return `Up to ${UPPER}`;
+    } else if (UPPER === Infinity) {
+      return `${LOWER} or more`;
+    } else {
+      return `${LOWER} - ${UPPER}`;
+    }
+  },
+
+  renderBracketRangeCells(taxableIncome, filingType, rateData) {
+    let classNames;
+    return Object.keys(rateData).map(fileType => {
+      if (filingType == fileType) {
+        classNames = 'selected-file-type';
+      } else {
+        classNames = '';
+      }
+      return (<td className={classNames}>{this.rateToString(rateData[fileType])}</td>);
+    });
+  },
+
+  renderBracketRows(taxableIncome, year, filingType) {
+    const yearData = BracketData[year];
+     let classNames;
+
+    return Object.keys(yearData).map(rate => {
+      const rateData = yearData[rate];
+      const range = rateData[filingType];
+
+      // TODO: not sure which end is inclusive or not
+      if (range.LOWER <= taxableIncome && range.UPPER > taxableIncome) {
+        classNames = 'selected-rate';
+      } else {
+        classNames = '';
+      }
+
+      return (
+        <tr className={classNames}>
+          <td>{rate}%</td>
+          {this.renderBracketRangeCells(taxableIncome, filingType, rateData)}
+        </tr>
+      );
+    });
+
+  },
+
+  renderBracketTable(taxableIncome, year, filingType) {
     return (
-      <h1>
-        <input type='number' valueLink={this.linkState('year')}/> <h1>Effective Tax Rate Calculator</h1>
-        {this.state.year} sdfs
-      </h1>
+      <table className="bracket-table">
+        <caption>2015 IRS Tax Brackets</caption>
+        <thead>
+          <tr>
+            <th>Tax rate</th>
+            <th>Single filers</th>
+            <th>Married filing jointly or qualifying widow(er)</th>
+            <th>Married filing separately</th>
+            <th>Head of household</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderBracketRows(taxableIncome, year, filingType)}
+        </tbody>
+      </table>
+    );
+  },
+
+  renderFilingTypeSelect() {
+    const options = Object.keys(this.filingTypes).map(key => {
+      return <option value={key}>{this.filingTypes[key]}</option>;
+    });
+    return (
+      <label>
+        Filing Status:
+        <select valueLink={this.linkState('filingType')}>
+          {options}
+        </select>
+      </label>
+    );
+  },
+
+  render() {
+    const {year, taxableIncome, filingType} = this.state;
+
+    return (
+      <div>
+        <h1>Effective Tax Rate Calculator (United States)</h1>
+        <label>
+          2015 Taxable Income:
+          <input type='number' valueLink={this.linkState('taxableIncome')}/>
+        </label>
+        {this.renderFilingTypeSelect()}
+        {this.renderBracketTable(taxableIncome, year, filingType)}
+      </div>
     );
   }
 });
---
+++ app/js/data/brackets.js
@@ -0,0 +1,132 @@
+export default {
+  2015: {
+    "10": {
+      SINGLE: {
+        LOWER: 0,
+        UPPER: 9225,
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 0,
+        UPPER: 18450
+      },
+      SEPARATE: {
+        LOWER: 0,
+        UPPER: 9225
+      },
+      HOH: {
+        LOWER: 0,
+        UPPER: 13150
+      }
+    },
+    "15": {
+      SINGLE: {
+        LOWER: 9226,
+        UPPER: 37450
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 18451,
+        UPPER: 74900
+      },
+      SEPARATE: {
+        LOWER: 9226,
+        UPPER: 37450
+      },
+      HOH: {
+        LOWER: 13151,
+        UPPER: 50200
+      }
+    },
+    "25": {
+      SINGLE: {
+        LOWER: 37451,
+        UPPER: 90750
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 74901,
+        UPPER: 151200
+      },
+      SEPARATE: {
+        LOWER: 37451,
+        UPPER: 75600
+      },
+      HOH: {
+        LOWER: 50201,
+        UPPER: 129600
+      }
+    },
+    "28": {
+      SINGLE: {
+        LOWER: 90751,
+        UPPER: 189300
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 151201,
+        UPPER: 230450
+      },
+      SEPARATE: {
+        LOWER: 75601,
+        UPPER: 115225
+      },
+      HOH: {
+        LOWER: 129601,
+        UPPER: 209850
+      }
+    },
+    "33": {
+      SINGLE: {
+        LOWER: 189301,
+        UPPER: 411500
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 230451,
+        UPPER: 411500
+      },
+      SEPARATE: {
+        LOWER: 115226,
+        UPPER: 205750
+      },
+      HOH: {
+        LOWER: 209851,
+        UPPER: 411500
+      }
+    },
+    "35": {
+      SINGLE: {
+        LOWER: 411501,
+        UPPER: 413200
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 411501,
+        UPPER: 464850
+      },
+      SEPARATE: {
+        LOWER: 205751,
+        UPPER: 232425
+      },
+      HOH: {
+        LOWER: 411501,
+        UPPER: 439000
+      }
+    },
+    "39.6": {
+      SINGLE: {
+        LOWER: 413201,
+        UPPER: Infinity
+      },
+      JOINT_OR_WIDOW: {
+        LOWER: 464851,
+        UPPER: Infinity
+      },
+      SEPARATE: {
+        LOWER: 232426,
+        UPPER: Infinity
+      },
+      HOH: {
+        LOWER: 439001,
+        UPPER: Infinity
+      }
+    },
+  }
+
+
+};
